90dc39473e1b5c0e2d51c96a9e90bdecb7879f69
brunoxd <bruno.mendes.costa@usp.br>
2018-06-06 22:33:37 -0300

    Update Script

--- debug/wannaCry.sh
+++ debug/wannaCry.sh
@@ -2,22 +2,41 @@
 
 # Usage: ./wannaCry.sh BEGIN_NUM_INSERTS END_NUM_INSERTS NUM_LOOPS
 
+# Variáveis de Ambiente
+
+# RANGES
 BEGIN_NUM_INSERTS=${1:-1}
 END_NUM_INSERTS=${2:-500}
 NUM_LOOPS=${3:-100}
-OUTPUT_FILENAME=errors.txt
 
-echo -n > $OUTPUT_FILENAME
+# FILENAMES
+OUTPUT=errors.txt
+#CLEAN_OUTPUT=cleanErrors.txt
+
+# PATHS
+DATABASE_SCHEMA_PATH=../examples/myDatabase.sql
+PROGRAM_PATH=../bllshtMyDatabase.py
+
+# Limpa o arquivo de output
+echo -n > $OUTPUT
+
+# Limpa o diretório dump
+rm -rf dump && mkdir dump
 
 # Realiza um teste exaustivo
 for i in `seq $BEGIN_NUM_INSERTS $END_NUM_INSERTS`; 
 do
 	for j in `seq 1 $NUM_LOOPS`;
-	do	
-		echo "Progress: $i $j"		
-		`python3m ../bllshtMyDatabase.py schemas.sql $i > insert.sql` >> OUTPUT_FILENAME
+	do			
+		echo -ne "Progress: [$i $j]\r"
+		`python3 $PROGRAM_PATH $DATABASE_SCHEMA_PATH $i > ./dump/insert$i$j.sql` >> $OUTPUT
+		#egrep "DELETE|ROLLBACK|CREATE|DROP|COMMIT|BEGIN|ALTER|INSERT" -v $OUTPUT > $CLEAN_OUTPUT
 	done
 done
 
-echo "Results:"
-cat $OUTPUT_FILENAME | sort | uniq -c | sort -k1 -n
+# Imprime os resultados
+#echo "Cleaned Outputs:"
+#cat $CLEAN_OUTPUT
+echo "Original Results:"
+cat $OUTPUT | sort | uniq -c | sort -k1 -n
+echo "Check ./dump/ for generated inserts."
